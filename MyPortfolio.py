58485571f2044694fab62218459ed30764541768
Fernando Pereira <103957365+ferpereira36@users.noreply.github.com>
2022-09-08 15:30:56 -0300

    Add files via upload

---
+++ MyPortfolio.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import *
+from PIL import Image, ImageTk
+import webbrowser
+from webbrowser import *
+from time import strftime
+from tkinter.messagebox import askyesno
+
+#   #   #   #   #   #   #   #   #   #   #   #   #   #
+
+
+#   #   #   Minha .py do Portfolio EM BREVE #   #   #
+
+
+#   #   #   #   #   #   #   #   #   #   #   #   #   #
